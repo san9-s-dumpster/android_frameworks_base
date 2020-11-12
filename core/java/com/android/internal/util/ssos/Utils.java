903ba12d76ff5fda80c56a6eaa17a49ce89a0476
Danny Lin <danny@kdrag0n.dev>
2020-11-12 22:24:50 +0100

    Custom utils: add Power menu action
    Change-Id: Ic496f607a540bb90ffe1b609900e463f3ef329ed
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/com/android/internal/util/ssos/Utils.java
+++ core/java/com/android/internal/util/ssos/Utils.java
@@ -168,7 +168,7 @@ public class Utils {
 
 
     public static void takeScreenshot(boolean full) {
-        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        final IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
         try {
             wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
         } catch (RemoteException e) {
@@ -502,5 +502,14 @@ public class Utils {
                 context.getSystemService(Context.AUDIO_SERVICE);
         audioMan.setRingerModeInternal(ringerMode);
         Toast.makeText(context, toastText, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void showPowerMenu() {
+        final IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.showGlobalActions();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
