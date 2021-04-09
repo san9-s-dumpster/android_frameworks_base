2b310729cfb73216e1ba854a83e8b9863780450a
Alexandru Scurtu <sasha.scurtu@gmail.com>
2021-04-10 00:21:01 +0300

    d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5869,6 +5869,11 @@ public final class Settings {
         public static final String TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK = "touchscreen_gesture_haptic_feedback";
 
         /**
+         * @hide
+         */
+        public static final String QS_HEADER_BACKGROUND = "qs_header_background";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
